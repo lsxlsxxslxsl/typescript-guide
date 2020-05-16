beca72cd05f64c992179b18a892bd89cf12557fb
刘思鑫 <fordreamxkhl@163.com>
2020-05-16 23:59:03 +0800

    chore: 只读属性

--- Interface.ts
+++ Interface.ts
@@ -1,11 +1,15 @@
 // 接口
 interface Person {
+  readonly id: number;
   name: string;
   age?: number;
-  [propName: string]: any; // 任意属性
+  [propName: string]: string | number; // 任意属性(一旦定义了任意属性，那么确定属性和可选属性的类型都必须是它的类型的子集)
 }
 
 let tom: Person = {
+  id: 89757,
   name: 'Tom',
-  gender: 'male'
+  age: 25,
+  gender: 'male',
 };
+tom.id = 9527;
