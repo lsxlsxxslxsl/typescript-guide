96824064719c1dfa102e10539a646df8932890ce
刘思鑫 <fordreamxkhl@163.com>
2020-05-20 00:56:16 +0800

    refactor: 组合模式优化

---
+++ src/analyzer.ts
@@ -0,0 +1,51 @@
+import cheerio from 'cheerio';
+import fs from 'fs';
+import { IAnalyzer } from './crowller';
+
+interface Course {
+  title: string;
+  count: number;
+}
+
+interface CourseResult {
+  time: number;
+  data: Course[];
+}
+
+interface Content {
+  [propName: number]: Course[];
+}
+
+export default class Analyzer implements IAnalyzer {
+  getCourseInfo(html: string) {
+    const $ = cheerio.load(html);
+    const courseList = $('.shizhan-course-wrap');
+    const courseInfoList: Course[] = [];
+    courseList.map((index, element) => {
+      const title = $(element).find('.shizan-name').attr('title') || '';
+      let i = $(element).find('.shizhan-info span').eq(0).hasClass('grade') ? 1 : 0;
+      const count = parseInt($(element).find('.shizhan-info span').eq(i).text());
+      courseInfoList.push({ title, count });
+    });
+
+    return {
+      time: new Date().getTime(),
+      data: courseInfoList
+    };
+  }
+
+  generateJsonContent(courseInfo: CourseResult, filePath: string) {
+    let fileContent: Content = {};
+    if (fs.existsSync(filePath)) {
+      fileContent = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+    }
+    fileContent[courseInfo.time] = courseInfo.data;
+    return fileContent;
+  }
+
+  public analyze(html: string, filePath: string) {
+    const courseInfo = this.getCourseInfo(html);
+    const fileContent = this.generateJsonContent(courseInfo, filePath);
+    return JSON.stringify(fileContent)
+  }
+}
--- src/crowller.ts
+++ src/crowller.ts
@@ -1,71 +1,36 @@
-import cheerio from 'cheerio';
 import fs from 'fs';
 import path from 'path';
 import superagent from 'superagent';
+import Analyzer from './analyzer';
 
-interface Course {
-  title: string | undefined;
-  count: number;
-}
-
-interface CourseResult {
-  time: number;
-  data: Course[];
-}
-
-interface Content {
-  [propName: number]: Course[];
+export interface IAnalyzer {
+  analyze: (html: string, filePath: string) => string;
 }
 
 class Crowller {
-  private url = `https://coding.imooc.com/`;
   private filePath = path.resolve(__dirname, '../data/course.json');
-
-  getCourseInfo(html: string) {
-    const $ = cheerio.load(html);
-    const courseList = $('.shizhan-course-wrap');
-    const courseInfoList: Course[] = [];
-    courseList.map((index, element) => {
-      const title = $(element).find('.shizan-name').attr('title');
-      let i = $(element).find('.shizhan-info span').eq(0).hasClass('grade') ? 1 : 0;
-      const count = parseInt($(element).find('.shizhan-info span').eq(i).text());
-      courseInfoList.push({ title, count });
-    });
-
-    return {
-      time: new Date().getTime(),
-      data: courseInfoList
-    };
-  }
 
   async getRawHtml() {
     const { text } = await superagent.get(this.url);
     return text;
   }
 
-  generateJsonContent(courseInfo: CourseResult) {
-    let fileContent: Content = {};
-    if (fs.existsSync(this.filePath)) {
-      fileContent = JSON.parse(fs.readFileSync(this.filePath, 'utf-8'));
-    }
-    fileContent[courseInfo.time] = courseInfo.data;
-    return fileContent
-  }
-
   writeFile(content: string) {
-    fs.writeFileSync(this.filePath, content)
+    fs.writeFileSync(this.filePath, content);
   }
 
   async initSpiderProcess() {
     const html = await this.getRawHtml();
-    const courseInfo = this.getCourseInfo(html);
-    const fileContent = this.generateJsonContent(courseInfo);
-    this.writeFile(JSON.stringify(fileContent)
+    const fileContent = analyzer.analyze(html, this.filePath);
+    this.writeFile(fileContent);
   }
 
-  constructor() {
+  constructor(private url: string, private analyzer: IAnalyzer) {
     this.initSpiderProcess();
   }
 }
 
-const crowller = new Crowller();
+const url = `https://coding.imooc.com/`;
+
+const analyzer = new Analyzer();
+new Crowller(url, analyzer);
