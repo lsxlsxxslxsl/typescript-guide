4cfc3e010ed0c34f67d45da6dd5d873f331ece4f
刘思鑫 <fordreamxkhl@163.com>
2020-05-22 01:41:50 +0800

    chore: keyof

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,33 @@
-import $ from 'jquery';
+interface Person {
+  name: string;
+  age: number;
+  gender: string;
+}
 
-$(function () {
-  $('body').html('<div>123</div>');
-  new $.fn.init();
+// type NAME = 'name';
+// key: 'name';
+// Person['name'];
+
+// type T = 'age'
+// key: 'age'
+// Person['age']
+
+// type T = 'gender'
+// key: 'gender'
+// Person['gender']
+
+class Teacher {
+  constructor(private info: Person) {}
+  getInfo<T extends keyof Person>(key: T): Person[T] {
+    return this.info[key];
+  }
+}
+
+const teacher = new Teacher({
+  name: 'dell',
+  age: 18,
+  gender: 'male'
 });
+
+const test = teacher.getInfo('name');
+console.log(test);
