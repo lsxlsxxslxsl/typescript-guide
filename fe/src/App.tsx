86d3df88167329661166f995f98683a673b622c7
刘思鑫 <fordreamxkhl@163.com>
2020-05-28 01:38:36 +0800

    feat: 添加路由

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,48 +1,17 @@
-import { Button, Form, Icon, Input } from 'antd';
-import React, { Component } from 'react';
-import './login.css';
+import React, { FC } from 'react';
+import { HashRouter, Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
 
-interface Props {
-  form: any;
-}
+const route: FC = () => {
+  return (
+    <div>
+      <HashRouter>
+        <Switch>
+          <Route path="/login" exact component={Login}></Route>
+        </Switch>
+      </HashRouter>
+    </div>
+  );
+};
 
-class NormalLoginForm extends Component<Props> {
-  handleSubmit = (e: any) => {
-    e.preventDefault();
-    this.props.form.validateFields((err: any, values: any) => {
-      if (!err) {
-        console.log('Received values of form: ', values);
-      }
-    });
-  };
-
-  render() {
-    const { getFieldDecorator } = this.props.form;
-    return (
-      <div className="login-page">
-        <Form onSubmit={this.handleSubmit} className="login-form">
-          <Form.Item>
-            {getFieldDecorator('password', {
-              rules: [{ required: true, message: '请输入密码!' }]
-            })(
-              <Input
-                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                type="password"
-                placeholder="请输入密码"
-              />
-            )}
-          </Form.Item>
-          <Form.Item>
-            <Button type="primary" htmlType="submit" className="login-form-button">
-              登录
-            </Button>
-          </Form.Item>
-        </Form>
-      </div>
-    );
-  }
-}
-
-const WrappedNormalLoginForm = Form.create({ name: 'normal_login' })(NormalLoginForm);
-
-export default WrappedNormalLoginForm
+export default route
---
+++ fe/src/pages/Login/index.tsx
@@ -0,0 +1,52 @@
+import { Button, Form, Icon, Input } from 'antd';
+import { WrappedFormUtils } from 'antd/lib/form/Form';
+import React, { Component } from 'react';
+import './login.css';
+
+interface FormFields {
+  password: string;
+}
+interface Props {
+  form: WrappedFormUtils<FormFields>;
+}
+
+class LoginForm extends Component<Props> {
+  handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+    });
+  };
+
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    return (
+      <div className="login-page">
+        <Form onSubmit={this.handleSubmit} className="login-form">
+          <Form.Item>
+            {getFieldDecorator('password', {
+              rules: [{ required: true, message: '请输入密码!' }]
+            })(
+              <Input
+                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                type="password"
+                placeholder="请输入密码"
+              />
+            )}
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" className="login-form-button">
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    );
+  }
+}
+
+const WrappedLoginForm = Form.create({ name: 'login' })(LoginForm);
+
+export default WrappedLoginForm;
