75db8a460db589715e8adf191b62023558e52cee
刘思鑫 <fordreamxkhl@163.com>
2020-05-28 01:05:55 +0800

    feat: 初始化antd

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,9 +1,48 @@
-import React from 'react';
+import { Button, Form, Icon, Input } from 'antd';
+import React, { Component } from 'react';
+import './login.css';
 
-function App() {
-  return (
-    <div>hello world！</div>
-  );
+interface Props {
+  form: any;
 }
 
-export default App;
+class NormalLoginForm extends Component<Props> {
+  handleSubmit = (e: any) => {
+    e.preventDefault();
+    this.props.form.validateFields((err: any, values: any) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+    });
+  };
+
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    return (
+      <div className="login-page">
+        <Form onSubmit={this.handleSubmit} className="login-form">
+          <Form.Item>
+            {getFieldDecorator('password', {
+              rules: [{ required: true, message: '请输入密码!' }]
+            })(
+              <Input
+                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                type="password"
+                placeholder="请输入密码"
+              />
+            )}
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" className="login-form-button">
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    );
+  }
+}
+
+const WrappedNormalLoginForm = Form.create({ name: 'normal_login' })(NormalLoginForm);
+
+export default WrappedNormalLoginForm
