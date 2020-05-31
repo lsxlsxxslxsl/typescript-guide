c0b4443ae584e59426587d072898d46c1131446e
刘思鑫 <fordreamxkhl@163.com>
2020-05-31 23:56:48 +0800

    chore: 引入echarts

--- fe/src/pages/Home/index.tsx
+++ fe/src/pages/Home/index.tsx
@@ -1,5 +1,6 @@
 import { Button, message } from 'antd';
 import axios from 'axios';
+import ReactEcharts from 'echarts-for-react';
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 import './home.css';
@@ -37,20 +38,88 @@ class Home extends Component {
     });
   };
 
+  getOption: () => echarts.EChartOption = () => {
+    return {
+      title: {
+        text: '折线图堆叠'
+      },
+      tooltip: {
+        trigger: 'axis'
+      },
+      legend: {
+        data: ['邮件营销', '联盟广告', '视频广告', '直接访问', '搜索引擎']
+      },
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+      },
+      toolbox: {
+        feature: {
+          saveAsImage: {}
+        }
+      },
+      xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          name: '邮件营销',
+          type: 'line',
+          stack: '总量',
+          data: [120, 132, 101, 134, 90, 230, 210]
+        },
+        {
+          name: '联盟广告',
+          type: 'line',
+          stack: '总量',
+          data: [220, 182, 191, 234, 290, 330, 310]
+        },
+        {
+          name: '视频广告',
+          type: 'line',
+          stack: '总量',
+          data: [150, 232, 201, 154, 190, 330, 410]
+        },
+        {
+          name: '直接访问',
+          type: 'line',
+          stack: '总量',
+          data: [320, 332, 301, 334, 390, 330, 320]
+        },
+        {
+          name: '搜索引擎',
+          type: 'line',
+          stack: '总量',
+          data: [820, 932, 901, 934, 1290, 1330, 1320]
+        }
+      ]
+    };
+  };
+
   render() {
     const { isLogin, loading } = this.state;
-    console.log('主页', isLogin, loading)
+    console.log('主页', isLogin, loading);
     if (isLogin) {
       if (!loading) {
         return (
           <div className="home-page">
-            <Button type="primary" style={{ marginLeft: '5px' }}>
-              爬取
-            </Button>
-            <Button type="primary">展示</Button>
-            <Button type="primary" onClick={this.handleLogoutClick}>
-              退出
-            </Button>
+            <div className="buttons">
+              <Button type="primary" style={{ marginLeft: '5px' }}>
+                爬取
+              </Button>
+              <Button type="primary">展示</Button>
+              <Button type="primary" onClick={this.handleLogoutClick}>
+                退出
+              </Button>
+            </div>
+            <ReactEcharts option={this.getOption()} />
           </div>
         );
       }
